30ca5d75f519024279d3dd1ee6f55db2b3ee7bcf
Nicholas Drzewiecki <drzewieckinicholas@gmail.com>
2025-05-11 19:57:04 -0700

    chore: configure Prettier

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  jsxSingleQuote: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+  singleQuote: true,
+};
+
+export default config;
