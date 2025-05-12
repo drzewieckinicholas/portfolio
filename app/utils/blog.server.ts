177ac5a3d52301dc4527c697860b64179cdd2b08
Nicholas Drzewiecki <drzewieckinicholas@gmail.com>
2025-05-11 22:00:18 -0700

    feat: cache blog posts

--- app/utils/blog.server.ts
+++ app/utils/blog.server.ts
@@ -1,10 +1,19 @@
 import { readdir, readFile } from 'fs/promises';
+import { LRUCache } from 'lru-cache';
 import { bundleMDX } from 'mdx-bundler';
 import path from 'path';
 
 import type { Post, PostFrontmatter } from '~/types';
 
 const POSTS_PATH = path.join(process.cwd(), 'app', 'posts');
+
+const postsCache = new LRUCache({
+  allowStale: false,
+  max: 10,
+  ttl: 1000 * 60 * 60,
+});
+
+const ALL_POSTS_CACHE_KEY = 'all-posts';
 
 export async function getPostsSlugs() {
   try {
@@ -21,10 +30,22 @@ export async function getPostsSlugs() {
 }
 
 export async function getPosts() {
+  const cachedPosts = postsCache.get(ALL_POSTS_CACHE_KEY) as Post[] | undefined;
+
+  if (cachedPosts) {
+    return cachedPosts;
+  }
+
   const slugs = await getPostsSlugs();
 
   const postsPromises = slugs.map(async (slug) => {
     try {
+      const cachedPost = postsCache.get(slug) as Post | undefined;
+
+      if (cachedPost) {
+        return { frontmatter: cachedPost.frontmatter, slug };
+      }
+
       const source = await readFile(
         path.join(POSTS_PATH, `${slug}.mdx`),
         'utf8',
@@ -45,15 +66,25 @@ export async function getPosts() {
 
   const posts = (await Promise.all(postsPromises)).filter(Boolean) as Post[];
 
-  return posts.sort((a, b) =>
+  const sortedPosts = posts.sort((a, b) =>
     a.frontmatter.date && b.frontmatter.date
       ? new Date(b.frontmatter.date).valueOf() -
         new Date(a.frontmatter.date).valueOf()
       : 0,
   );
+
+  postsCache.set(ALL_POSTS_CACHE_KEY, sortedPosts);
+
+  return sortedPosts;
 }
 
 export async function getPost(slug: string) {
+  const cachedPost = postsCache.get(slug);
+
+  if (cachedPost) {
+    return cachedPost;
+  }
+
   const filePath = path.join(POSTS_PATH, `${slug}.mdx`);
 
   try {
@@ -64,7 +95,11 @@ export async function getPost(slug: string) {
       source,
     });
 
-    return { code, frontmatter, slug };
+    const post = { code, frontmatter, slug };
+
+    postsCache.set(slug, post);
+
+    return post;
   } catch (error) {
     console.error('Failed to load post', error);
 
