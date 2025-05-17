599c9e4ee462f9543f844dc04d7e9a61e60ddb6c
Nicholas Drzewiecki <drzewieckinicholas@gmail.com>
2025-05-16 20:20:26 -0700

    feat: add list styles

--- app/components/NavLink/index.tsx
+++ app/components/NavLink/index.tsx
@@ -20,8 +20,11 @@ export default function NavLink({
     <RemixNavLink
       className={({ isActive, isPending, isTransitioning }) => {
         const classes = clsx(
-          variant === 'pill' && 'nav-pill',
-          variant === 'pill' && isActive && 'active',
+          variant === 'pill' &&
+            'rounded-xl px-2 py-1 text-inherit no-underline transition-colors hover:bg-white hover:bg-opacity-10',
+          variant === 'pill' &&
+            isActive &&
+            'bg-white bg-opacity-10 text-rose-300',
 
           typeof className === 'function'
             ? className({ isActive, isPending, isTransitioning })
