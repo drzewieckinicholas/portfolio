653fd13a3c23165b62267c19136265712709f2cc
Nicholas Drzewiecki <drzewieckinicholas@gmail.com>
2025-05-27 00:31:03 -0700

    refactor: add separate blog layout route

--- app/routes/_app.tsx
+++ app/routes/_app.tsx
@@ -1,33 +1,17 @@
 import { Outlet, useMatches } from '@remix-run/react';
 
 import { Header, WithNav } from '~/components';
-import avatarImage from '~/images/avatar.webp';
 import type { Match } from '~/types';
 
-export default function App() {
+export default function AppLayout() {
   const matches = useMatches() as Match[];
-
   const match = matches[matches.length - 1];
-  const loaderData = match.data;
-  const postTitle = loaderData?.post?.frontmatter?.title;
-  const postDate = loaderData?.post?.frontmatter?.date;
-  const heading = postTitle || match.handle.heading;
-
-  const date = postDate ? new Date(postDate) : undefined;
-
-  const isBlogPost = Boolean(loaderData?.post);
-  const imageAlt = isBlogPost ? 'Avatar of Nicholas' : undefined;
-  const imageUrl = isBlogPost ? avatarImage : undefined;
+  const heading = match.handle.heading;
 
   return (
     <>
       <WithNav />
-      <Header
-        date={date}
-        heading={heading}
-        imageAlt={imageAlt}
-        imageUrl={imageUrl}
-      />
+      <Header heading={heading} />
       <main>
         <Outlet />
       </main>
