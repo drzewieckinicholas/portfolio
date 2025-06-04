011ba0256a8a6155a61950c0735f7fa47d09ee05
Nicholas Drzewiecki <drzewieckinicholas@gmail.com>
2025-06-03 21:28:44 -0700

    refactor: revert back to a single pathless layout route

--- app/routes/_app.tsx
+++ app/routes/_app.tsx
@@ -1,17 +1,32 @@
 import { Outlet, useMatches } from '@remix-run/react';
 
 import { Header, WithNav } from '~/components';
+import avatarImage from '~/images/avatar.webp';
 import type { Match } from '~/types';
 
 export default function AppLayout() {
   const matches = useMatches() as Match[];
   const match = matches[matches.length - 1];
-  const heading = match.handle.heading;
+  const loaderData = match.data;
+
+  const isBlogRoute = Boolean(loaderData?.post);
+
+  const heading = loaderData?.post?.frontmatter?.title || match.handle.heading;
+  const date = loaderData?.post?.frontmatter?.date
+    ? new Date(loaderData.post.frontmatter.date)
+    : undefined;
 
   return (
     <>
       <WithNav />
-      <Header heading={heading} />
+      <Header
+        {...(isBlogRoute && {
+          date,
+          imageAlt: 'Avatar of Nicholas',
+          imageUrl: avatarImage,
+        })}
+        heading={heading}
+      />
       <main>
         <Outlet />
       </main>
